951688a1d2e80f4428400a64fa0f112de727fd7e
cmills39 <58275422+cmills39@users.noreply.github.com>
2020-05-03 13:23:44 -0400

    Add files via upload

---
+++ Instructions/StarterCode/static/js/BBHW.js
@@ -0,0 +1,66 @@
+function metadata(sample){
+    d3.json("samples.json").then((data)=>{
+        var metadata= data.metadata;
+        var array= metadata.filter(object =>object.id==sample);
+        var result= array [0];
+        var BBpanel= d3.select("#sample-metadata");
+        BBpanel.html("");
+        Object.entries(result).forEach(([key,value]) => {
+            BBpanel.append("h6").text(`${key}:${value}`)
+        });
+    });
+}
+
+function createchart(sample){
+    d3.json("samples.json").then((data)=>{
+        var samples= data.samples;
+        var array= samples.filter(object =>object.id==sample);
+        var result= array [0];
+        var otu_ids= result.otu_ids;
+        var otu_labels=result.otu_labels;
+        var sample_values=result.sample_values;
+        var bardata=[
+            {
+                y:otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse(),
+                x:sample_values.slice(0,10).reverse(),
+                text:otu_labels.slice(0,10).reverse(),
+                type:"bar",
+                orientation:'h'
+
+            }
+        ];
+        var barlayout={
+            title:"BBdata"
+        };
+        Plotly.newPlot("bar",bardata,barlayout);
+    });
+}
+
+
+function init(){
+    var selectedID= d3.select("#selDataset");
+    d3.json("samples.json").then((data)=>{
+        var names= data.names;
+        names.forEach((sample )=>{
+            selectedID.append("option")
+            .text(sample)
+            .property("value",sample);
+
+
+        });
+
+        var firstsample= names[0];
+        metadata(firstsample);
+
+        createchart(firstsample);
+
+});
+}
+
+init()
+
+function optionChanged(sample){
+    metadata(sample);
+    createchart(sample);
+}
+
